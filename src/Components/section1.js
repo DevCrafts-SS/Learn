592e5c05d1fb8c6d664d96f34621205ca71b8567
DevCrafts-SS <sakshams346@gmail.com>
2024-03-19 21:50:07 +0530

    React First Project

---
+++ src/Components/section1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Css/section1.css';
+
+function Section1() {
+  return (
+    <div id='about' className='poster'>
+      <img src="../../img/hamburger.jpg" alt="Hamburger" />
+      <h1>Le Catering</h1>
+    </div>
+  );
+}
+
+export default Section1;
